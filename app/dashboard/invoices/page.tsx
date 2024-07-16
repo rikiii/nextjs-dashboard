8ac28efa4ea820fb78e0a6d78dab4453095a3a73
riki <rikinotricky@gmail.com>
2024-07-16 11:32:00 +0900

    update

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,9 +1,16 @@
-import Table from "@/app/ui/customers/table";
-import { lusitana } from "@/app/ui/fonts";
-import { CreateInvoice } from "@/app/ui/invoices/buttons";
-import Search from "@/app/ui/search";
-import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
-import { Suspense } from "react";
+import { lusitana } from '@/app/ui/fonts';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import Search from '@/app/ui/search';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import Table from '@/app/ui/invoices/table';
+import { fetchInvoicesPages } from '@/app/lib/data';
+import Pagination from '@/app/ui/invoices/pagination';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default async function Page({
   searchParams,
@@ -13,9 +20,10 @@ export default async function Page({
     page?: string;
   };
 }) {
-  const query = searchParams?.query || "";
+  const query = searchParams?.query || '';
   const currentPage = Number(searchParams.page) || 1;
 
+  const totalPages = await fetchInvoicesPages(query);
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -29,7 +37,7 @@ export default async function Page({
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
